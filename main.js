4b324c85ef401ae3f564b3a42e05f2e767c0b8e6
Ebraheem1 <ebraheem.mouhamed@gmail.com>
2018-03-15 18:36:29 +0200

    Five Fingers Details

--- main.js
+++ main.js
@@ -15,18 +15,56 @@ var Controller = new Leap.Controller(options);
 var controller = Leap.loop(Controller, function(frame) {
     
     if(frame.hands.length > 0){
-        thumbDetails(frame.hands[0].thumb);
+        thumbFinger(frame.hands[0].thumb);
+        indexFinger(frame.hands[0].indexFinger);
+        middleFinger(frame.hands[0].middleFinger);
+        ringFinger(frame.hands[0].ringFinger);
         pinkyFinger(frame.hands[0].pinky);
     }
   });     
 
-var thumbDetails = function(thumb){
-    var ditsalDirection = thumb.distal.direction();
-    var proximalDirection = thumb.proximal.direction();
-    var metacarpalDirection = thumb.metacarpal.direction();
-    var upAngle = Math.acos(Leap.vec3.dot(ditsalDirection, proximalDirection)) * (180 / Math.PI);
-    var downAngle = Math.acos(Leap.vec3.dot(proximalDirection, metacarpalDirection)) * (180 / Math.PI);
+var thumbFinger = function(thumb){
+    var thumbDistal = thumb.distal.direction();
+    var thumbProximal = thumb.proximal.direction();
+    var thumbMetacarpal = thumb.metacarpal.direction();
+    var distal_proximal = Math.acos(Leap.vec3.dot(thumbDistal, thumbProximal)) * (180 / Math.PI);
+    var proximal_metacarpal = Math.acos(Leap.vec3.dot(thumbProximal, thumbMetacarpal)) * (180 / Math.PI);
+};
 
+var indexFinger = function(index)
+{
+    var indexDistal = index.distal.direction();
+    var indexMedial = index.medial.direction();
+    var indexProximal = index.proximal.direction();
+    var indexMetacarpal = index.metacarpal.direction();
+  
+    var distal_medial = Math.acos(Leap.vec3.dot(indexDistal, indexMedial)) * (180 / Math.PI);
+    var medial_proximal = Math.acos(Leap.vec3.dot(indexMedial, indexProximal)) * (180 / Math.PI);
+    var proximal_metacarpal = Math.acos(Leap.vec3.dot(indexProximal, indexMetacarpal)) * (180 / Math.PI);
+};
+
+var middleFinger = function(middle)
+{
+    var middleDistal = middle.distal.direction();
+    var middleMedial = middle.medial.direction();
+    var middleProximal = middle.proximal.direction();
+    var middleMetacarpal = middle.metacarpal.direction();
+  
+    var distal_medial = Math.acos(Leap.vec3.dot(middleDistal, middleMedial)) * (180 / Math.PI);
+    var medial_proximal = Math.acos(Leap.vec3.dot(middleMedial, middleProximal)) * (180 / Math.PI);
+    var proximal_metacarpal = Math.acos(Leap.vec3.dot(middleProximal, middleMetacarpal)) * (180 / Math.PI);
+};
+
+var ringFinger = function(ring)
+{
+    var ringDistal = ring.distal.direction();
+    var ringMedial = ring.medial.direction();
+    var ringProximal = ring.proximal.direction();
+    var ringMetacarpal = ring.metacarpal.direction();
+  
+    var distal_medial = Math.acos(Leap.vec3.dot(ringDistal, ringMedial)) * (180 / Math.PI);
+    var medial_proximal = Math.acos(Leap.vec3.dot(ringMedial, ringProximal)) * (180 / Math.PI);
+    var proximal_metacarpal = Math.acos(Leap.vec3.dot(ringProximal, ringMetacarpal)) * (180 / Math.PI);
 };
 
 
@@ -37,11 +75,11 @@ var pinkyFinger = function(pinky) {
   var pinkyProximal = pinky.proximal.direction();
   var pinkyMetacarpal = pinky.metacarpal.direction();
 
-  var distal_medial = Math.acos(Leap.vec3.dot(pinkyDistal, pinkyMedial));
-  var medial_proximal = Math.acos(Leap.vec3.dot(pinkyMedial, pinkyProximal));
-  var proximal_metacarpal = Math.acos(Leap.vec3.dot(pinkyProximal, pinkyMetacarpal));
+  var distal_medial = Math.acos(Leap.vec3.dot(pinkyDistal, pinkyMedial)) * (180 / Math.PI);
+  var medial_proximal = Math.acos(Leap.vec3.dot(pinkyMedial, pinkyProximal)) * (180 / Math.PI);
+  var proximal_metacarpal = Math.acos(Leap.vec3.dot(pinkyProximal, pinkyMetacarpal)) * (180 / Math.PI);
 
-  console.log("Angle between distal and medial: " + distal_medial * 180 / Math.PI);
-  console.log("Angle between medial and proximal: " + medial_proximal * 180 / Math.PI);
-  console.log("Angle between Proximal and Metacarpal: " + proximal_metacarpal * 180 / Math.PI);
+  console.log("Angle between distal and medial: " + distal_medial);
+  console.log("Angle between medial and proximal: " + medial_proximal);
+  console.log("Angle between Proximal and Metacarpal: " + proximal_metacarpal);
 }; 
