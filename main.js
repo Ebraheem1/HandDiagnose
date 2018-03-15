3d09fb7ecca3dd50132590f4f57db3b8911c525d
Ebraheem1 <ebraheem.mouhamed@gmail.com>
2018-03-15 18:18:19 +0200

    Renaming the variables with the right names

--- main.js
+++ main.js
@@ -12,25 +12,36 @@ var options = {
 var Controller = new Leap.Controller(options);
 
 
+var controller = Leap.loop(Controller, function(frame) {
+    
+    if(frame.hands.length > 0){
+        thumbDetails(frame.hands[0].thumb);
+        pinkyFinger(frame.hands[0].pinky);
+    }
+  });     
+
 var thumbDetails = function(thumb){
     var ditsalDirection = thumb.distal.direction();
     var proximalDirection = thumb.proximal.direction();
     var metacarpalDirection = thumb.metacarpal.direction();
     var upAngle = Math.acos(Leap.vec3.dot(ditsalDirection, proximalDirection)) * (180 / Math.PI);
     var downAngle = Math.acos(Leap.vec3.dot(proximalDirection, metacarpalDirection)) * (180 / Math.PI);
+
 };
 
-var pinkyFinger = function(pinky) {
-  var indexDistal = pinky.distal.direction();
-  var indexMedial = pinky.medial.direction();
-  var indexProximal = pinky.proximal.direction();
-  var indexMetacarpal = pinky.metacarpal.direction();
 
-  var distal_medial = Math.acos(Leap.vec3.dot(indexDistal, indexMedial));
-  var medial_proximal = Math.acos(Leap.vec3.dot(indexMedial, indexProximal));
-  var proximal_metacarpal = Math.acos(Leap.vec3.dot(indexProximal, indexMetacarpal));
+
+var pinkyFinger = function(pinky) {
+  var pinkyDistal = pinky.distal.direction();
+  var pinkyMedial = pinky.medial.direction();
+  var pinkyProximal = pinky.proximal.direction();
+  var pinkyMetacarpal = pinky.metacarpal.direction();
+
+  var distal_medial = Math.acos(Leap.vec3.dot(pinkyDistal, pinkyMedial));
+  var medial_proximal = Math.acos(Leap.vec3.dot(pinkyMedial, pinkyProximal));
+  var proximal_metacarpal = Math.acos(Leap.vec3.dot(pinkyProximal, pinkyMetacarpal));
 
   console.log("Angle between distal and medial: " + distal_medial * 180 / Math.PI);
   console.log("Angle between medial and proximal: " + medial_proximal * 180 / Math.PI);
   console.log("Angle between Proximal and Metacarpal: " + proximal_metacarpal * 180 / Math.PI);
-}
+}; 
