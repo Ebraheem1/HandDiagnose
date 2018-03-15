f2fd2748b5a6db4e67f82d0f0d3bea81ab83302b
Ebraheem1 <ebraheem.mouhamed@gmail.com>
2018-03-15 17:51:44 +0200

    Thumb measurements

--- main.js
+++ main.js
@@ -10,3 +10,14 @@ var options = {
 
   //Just to adjust the options no more
 var Controller = new Leap.Controller(options);
+
+  
+  
+
+var thumbDetails = function(thumb){
+    var ditsalDirection = thumb.distal.direction();
+    var proximalDirection = thumb.proximal.direction();
+    var metacarpalDirection = thumb.metacarpal.direction();
+    var upAngle = Math.acos(Leap.vec3.dot(ditsalDirection, proximalDirection)) * (180 / Math.PI);
+    var downAngle = Math.acos(Leap.vec3.dot(proximalDirection, metacarpalDirection)) * (180 / Math.PI);
+};
