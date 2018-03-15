67c01972b5e5dc6fa4ed47e5d212f30cd28f0d93
yasmineamr <yasmineabdulfattah@gmail.com>
2018-03-15 16:52:53 +0100

    Measures angle between Pinky Finger joints

--- main.js
+++ main.js
@@ -10,3 +10,18 @@ var options = {
 
   //Just to adjust the options no more
 var Controller = new Leap.Controller(options);
+
+var pinkyFinger = function(pinky) {
+  var indexDistal = pinky.distal.direction();
+  var indexMedial = pinky.medial.direction();
+  var indexProximal = pinky.proximal.direction();
+  var indexMetacarpal = pinky.metacarpal.direction();
+
+  var distal_medial = Math.acos(Leap.vec3.dot(indexDistal, indexMedial));
+  var medial_proximal = Math.acos(Leap.vec3.dot(indexMedial, indexProximal));
+  var proximal_metacarpal = Math.acos(Leap.vec3.dot(indexProximal, indexMetacarpal));
+
+  console.log("Angle between distal and medial: " + distal_medial * 180 / Math.PI);
+  console.log("Angle between medial and proximal: " + medial_proximal * 180 / Math.PI);
+  console.log("Angle between Proximal and Metacarpal: " + proximal_metacarpal * 180 / Math.PI);
+}
